5283760c573e55d7aeee047821766268556b1a59
Avi Sharan <65461975+avisharan23@users.noreply.github.com>
2022-07-22 12:47:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,25 +19,25 @@ document.addEventListener("keydown",function(event){
 function makeSound(key){
     switch (key) {
         case 'w':
-            var audio = new Audio('sounds/tom-1.mp3');
+            var audio = new Audio('/sounds/tom-1.mp3');
             audio.play();
             break;
 
         case 'a':
-            var audio = new Audio('sounds/tom-2.mp3');
+            var audio = new Audio('/sounds/tom-2.mp3');
             audio.play();
             break;
         
         case 's':
-            var audio = new Audio('sounds/tom-3.mp3');
+            var audio = new Audio('/sounds/tom-3.mp3');
             audio.play();
             break;
         case 'd':
-            var audio = new Audio('sounds/tom-4.mp3');
+            var audio = new Audio('/sounds/tom-4.mp3');
             audio.play();
             break;
         case 'j':
-            var audio = new Audio('sounds/kick-bass.mp3');
+            var audio = new Audio('/sounds/kick-bass.mp3');
             audio.play();
             break;
         case 'k':
@@ -45,7 +45,7 @@ function makeSound(key){
             audio.play();
             break;
         case 'l':
-            var audio = new Audio('sounds/snare.mp3');
+            var audio = new Audio('/sounds/snare.mp3');
             audio.play();
             break;
         
